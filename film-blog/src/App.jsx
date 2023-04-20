44405f2702cb139f5f014f9401f7b887aecce94a
Simon-Bertram <simonbertram@hotmail.com>
2023-04-20 11:59:46 +0100

    Initial base styling

--- film-blog/src/App.jsx
+++ film-blog/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './styles/main.scss'
 
 function App() {
 
   return (
     <div className="App">
       <h1>Welcome to my blog!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ad ratione quod exercitationem inventore qui, quis vitae provident odit?</p>
     </div>
   )
 }
