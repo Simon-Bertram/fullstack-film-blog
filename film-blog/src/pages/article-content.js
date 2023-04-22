e9f55f655ecd5252191d1fe75268cbf4fffccc3e
Simon-Bertram <simonbertram@hotmail.com>
2023-04-22 16:17:54 +0100

    Articles and ArticlesListPage now mapping content from js file, styling articles

---
+++ film-blog/src/pages/article-content.js
@@ -0,0 +1,75 @@
+const articles = [
+  {
+      name: 'star-wars',
+      title: 'Star Wars',
+      content: [
+          `Welcome! Today we're going to be talking about the Star Wars. We'll be discussing some topics such as proin congue
+          ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+          Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+          non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+          eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+          sodales purus euismod.`,
+          `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+          Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+          nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+          sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+          interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+          consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+          `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+          Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+          nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+          sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+          interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+          consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+      ]
+  },    {
+      name: 'cocaine-bear',
+      title: 'Cocaine Bear',
+      content: [
+          `In this article, we're going to be talking about a modern high-concept (thin-concept). We'll be discussing some topics such as proin congue
+          ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+          Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+          non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+          eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+          sodales purus euismod.`,
+          `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+          Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+          nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+          sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+          interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+          consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+          `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+          Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+          nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+          sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+          interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+          consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+      ]
+  },     {
+      name: 'aftersun',
+      title: 'Aftersun',
+      content: [
+          `Today is the day I talk about something which scares most people: holidays with your children.
+          In reality, I'm not sure why people have such a hard time with proin congue
+          ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+          Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+          non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+          eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+          sodales purus euismod.`,
+          `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+          Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+          nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+          sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+          interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+          consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+          `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+          Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+          nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+          sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+          interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+          consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+      ]
+  },  
+];
+
+export default articles;
