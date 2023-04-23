d34df403ef829fdc73cd476988b346c79a908279
Simon-Bertram <simonbertram@hotmail.com>
2023-04-23 15:52:50 +0100

    Import Mongodb and create endpoint to connect and get an article

--- film-blog-backend/src/server.js
+++ film-blog-backend/src/server.js
@@ -1,20 +1,52 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
-let articlesInfo = [
-  { name: 'star-wars', upvotes: 0, },
-  { name: 'cocaine-bear', upvotes: 0, },
-  { name: 'aftersun', upvotes: 0, }
-]
+// let articlesInfo = [
+//   { name: 'star-wars', upvotes: 0, comments: [] },
+//   { name: 'cocaine-bear', upvotes: 0, comments: [] },
+//   { name: 'aftersun', upvotes: 0, comments: [] }
+// ]
 
 const app = express();
 app.use(express.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+  const { name } = req.params;
+
+  const client = new MongoClient('mongodb://127.0.0.1:27017');
+  await client.connect();
+
+  const db = client.db('film-blog-db');
+
+  const article = await db.collection('articles').findOne({ name });
+
+  if (article) {
+    res.json(article);
+  } else {
+    res.sendStatus(404).send('Article not found.');
+  }
+});
 
 app.put('/api/articles/:name/upvote', (req, res) => {
   const { name } = req.params;
   const article = articlesInfo.find(article => article.name === name);
   if (article) {
     article.upvotes += 1;
-    res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    res.send(`The ${name} article now has ${article.upvotes} upvotes!`);
+  } else {
+    res.send('That article doesn\'t exist.');
+  }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find(article => article.name === name);
+
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
   } else {
     res.send('That article doesn\'t exist.');
   }
