e4e725838f52585da37a177d095ff6cb11546bd2
Simon-Bertram <simonbertram@hotmail.com>
2023-04-25 09:19:39 +0100

    Add state to article page and hard-code setting upvotes

--- film-blog-backend/src/server.js
+++ film-blog-backend/src/server.js
@@ -1,11 +1,5 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
-
-// let articlesInfo = [
-//   { name: 'star-wars', upvotes: 0, comments: [] },
-//   { name: 'cocaine-bear', upvotes: 0, comments: [] },
-//   { name: 'aftersun', upvotes: 0, comments: [] }
-// ]
+import { db, connectToDb } from './db.js';
 
 const app = express();
 app.use(express.json());
@@ -13,27 +7,19 @@ app.use(express.json());
 app.get('/api/articles/:name', async (req, res) => {
   const { name } = req.params;
 
-  const client = new MongoClient('mongodb://127.0.0.1:27017');
-  await client.connect();
-
-  const db = client.db('film-blog-db');
-
   const article = await db.collection('articles').findOne({ name });
 
   if (article) {
     res.json(article);
   } else {
-    res.sendStatus(404).send('Article not found.');
+    return
+    // res.sendStatus(404).send('Article not found.');
   }
 });
 
 app.put('/api/articles/:name/upvote', async (req, res) => {
   const { name } = req.params;
-  
-  const client = new MongoClient('mongodb://127.0.0.1:27017');
-  await client.connect();
 
-  const db = client.db('film-blog-db');
   await db.collection('articles').updateOne({ name }, {
     $inc: { upvotes: 1 },
   });
@@ -46,20 +32,25 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
   }
 });
 
-app.post('/api/articles/:name/comments', (req, res) => {
+app.post('/api/articles/:name/comments', async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
 
-  const article = articlesInfo.find(article => article.name === name);
+  await db.collection('articles').updateOne({ name }, {
+    $push: { comments: { postedBy, text } },
+  });
+  const article = await db.collection('articles').findOne({ name });
 
   if (article) {
-    article.comments.push({ postedBy, text });
     res.send(article.comments);
   } else {
     res.send('That article doesn\'t exist.');
   }
 });
 
-app.listen(8000, () => {
-  console.log('Server is listening on port 8000');
+connectToDb(() => {
+  console.log('Successfully connected to database.');
+  app.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+  });    
 });
