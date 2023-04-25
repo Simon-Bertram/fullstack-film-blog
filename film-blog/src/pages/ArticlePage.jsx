fd730661ee635aed4d7e9bff17d7ab56c636d5fd
Simon-Bertram <simonbertram@hotmail.com>
2023-04-25 09:42:17 +0100

    Add CommentsList component

--- film-blog/src/pages/ArticlePage.jsx
+++ film-blog/src/pages/ArticlePage.jsx
@@ -1,17 +1,24 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
 
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  const { articleId } = useParams();
 
   useEffect(() => {
-    setArticleInfo({ upvotes: 3, comments: [] });
-  });
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`http://localhost:8000/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    }
 
-  const { articleId } = useParams();
+    loadArticleInfo();
+  }, []);
+
   const article = articles.find(article => article.name === articleId);
 
   if (!article) {
