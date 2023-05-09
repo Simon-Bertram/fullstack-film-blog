85ceb715cf4ee84b702143b0f1a1b5abaceafe3d
Simon-Bertram <simonbertram@hotmail.com>
2023-05-09 18:01:33 +0100

    Protect endpoints using auth tokens

--- film-blog-backend/src/server.js
+++ film-blog-backend/src/server.js
@@ -13,12 +13,29 @@ admin.initializeApp({
 const app = express();
 app.use(express.json());
 
+app.use(async (req, res, next) => {
+  const { authtoken } = req.headers;
+
+  if (authtoken) {
+    try {
+      req.user = await admin.auth().verifyIdToken(authtoken);
+    } catch (error) {
+      res.sendStatus(400);
+    }
+  }  
+
+  next();
+});
+
 app.get('/api/articles/:name', async (req, res) => {
   const { name } = req.params;
+  const { uid } = req.user;
 
   const article = await db.collection('articles').findOne({ name });
 
   if (article) {
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpVote = uid && !upvoteIds.include(uid);
     res.json(article);
   } else {
     res.send('Article not found.');
