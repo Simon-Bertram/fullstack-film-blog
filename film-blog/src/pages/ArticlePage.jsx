376a465ff7fcc69fe82ee657355320468d2ef253
Simon-Bertram <simonbertram@hotmail.com>
2023-05-09 17:09:36 +0100

    Build login form and a custom auth hook

--- film-blog/src/pages/ArticlePage.jsx
+++ film-blog/src/pages/ArticlePage.jsx
@@ -4,12 +4,14 @@ import axios from 'axios';
 import articles from './article-content';
 import CommentsList from '../components/CommentsList';
 import AddCommentForm from '../components/AddCommentForm';
+import useUser from '../hooks/useUser';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
-
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams();
+
+  const { user, isLoading } = useUser();
 
   useEffect(() => {
     const loadArticleInfo = async () => {
@@ -37,16 +39,22 @@ const ArticlePage = () => {
     <>
       <h1>{article.title}</h1>
       <div className='upvotes-section'>
-        <button onClick={addUpvote}>Upvote</button>
+        {user
+          ? <button onClick={addUpvote}>Upvote</button>
+          : <button>Log in to upvote</button>
+        }
         <p>This article has {articleInfo.upvotes} upvote(s).</p>
       </div>
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
-      <AddCommentForm 
-        articleName={articleId} 
-        onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} 
-      />
+      {user
+        ? <AddCommentForm 
+            articleName={articleId} 
+            onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} 
+          />
+        : <button>Log in to add a comment</button>
+      }
       <CommentsList comments={articleInfo.comments} />
     </>
   )
--- film-blog/src/pages/LoginPage.jsx
+++ film-blog/src/pages/LoginPage.jsx
@@ -1,19 +1,57 @@
 import { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+  const navigate = useNavigate();
+
+  const logIn = async () => {
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      navigate('/articles');
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
   return (
     <>
       <h1>Log In</h1>
+      {error && <p className="error">{error}</p>}
       <form action="">
         <fieldset>
-          <input type="text" />
-          <input type="password" />
-          <button type="submit">Log In</button>
-          
+          <legend>Login</legend>
+          <ul>
+            <li className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input 
+                type="text" 
+                id="username"
+                placeholder="Your email address"
+                value={email}
+                onChange={e => setEmail(e.target.value)} 
+              />
+            </li>
+            <li className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input 
+                type="password" 
+                id="password" 
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </li>
+          </ul>
+          <button 
+           type="submit"
+           onClick={logIn}
+          >
+            Log In</button>
+          <Link to="/create-account">Don't have an account? Create one here</Link>
         </fieldset>
       </form>
     </>
