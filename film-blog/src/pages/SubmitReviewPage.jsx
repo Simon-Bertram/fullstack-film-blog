f1749e307d42efa2bbd794a189a03e5f53681c22
Simon-Bertram <simonbertram@hotmail.com>
2023-05-29 19:06:34 +0100

    Reformat form, label, input elements in SubmitReviewPage

--- film-blog/src/pages/SubmitReviewPage.jsx
+++ film-blog/src/pages/SubmitReviewPage.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
 
 const SubmitReview = () => {
+  const [user, setUser] = useState(null);
+  const [title, setTitle] = useState('');
   const [name, setName] = useState('');
   const [review, setReview] = useState('');
 
-  const addReview = (event) => {
-    event.preventDefault();
-    const formData = {
-      name,
-      review
-    };
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+    
+    // Amend the name in a URL-friendly formatted way
+    const name = replaceSpacesWithDashes(title);
+    formData.append('name', name);
     console.log(formData);
+
   }
 
   const replaceSpacesWithDashes = (string) => {
@@ -26,36 +34,41 @@ const SubmitReview = () => {
   return (
     <>
       <h1>Submit a review</h1>
-      <form 
-      className="review-form"
-      action=''
-      method='post'
-      >
+      <form className="review-form" method='post' onSubmit={handleSubmit}>
         <fieldset>
-          <legend>Add a Comment</legend>
+          <legend>Submit a review</legend>
           <ul>
             <li className="form-group">
-              <label htmlFor="name">Film Title:</label>
-              <input 
-                type="text"
-                value={name}
-                onChange={event => setName(event.target.value)}
-              />
+              <label>
+                Film Title: 
+                <input 
+                  name='title' 
+                  value={title}
+                  onChange={event => setTitle(event.target.value)} 
+                />
+              </label>
             </li>
             <li className="form-group">
-              <label htmlFor="review-text">Review:</label>
-              <textarea 
+              <label>
+                Review:
+                <textarea 
+                  name='reviewContent' 
+                  value={review}
+                  onChange={event => setReview(event.target.value)}
+                  cols={60} 
+                  rows={16}
+                  minLength="200"
+                />
+              </label>
+              {/* <textarea 
                 value={review}
                 onChange={setReview}
                 name="" 
                 id="review-text" 
-                cols="60" 
-                rows="16">
-              </textarea>
+                >
+              </textarea> */}
             </li>
-            <li>
-              <button type='submit' onClick={addReview}>Submit the review</button>
-            </li>
+            <button type='submit'>Submit the review</button>
           </ul>
         </fieldset>  
       </form>
