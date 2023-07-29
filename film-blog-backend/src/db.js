88ecbc79e3dd2c48344d3283577953fd19afb39d
Simon-Bertram <simonbertram@hotmail.com>
2023-07-29 18:19:32 +0100

    Incorporate a try/catch block in db.js

--- film-blog-backend/src/db.js
+++ film-blog-backend/src/db.js
@@ -3,10 +3,15 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(callback) {
-  const client = new MongoClient('mongodb://127.0.0.1:27017');
-  await client.connect();
-  db = client.db('film-blog-db');
-  callback();
+  try {
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+    db = client.db('film-blog-db');
+    callback();
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+    process.exit(1);
+  }
 }
 
 export {
